6e900a00d31585a836c97173ec8e24f83f1fcc8d
Gsingh1575 <108575808+Gsingh1575@users.noreply.github.com>
2023-07-10 22:12:39 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class string {
+    public static void main(String[] args) {
+//        String s1="Hello";
+//        String s2="Hello";
+//        String s3= new String("Hello");
+//        s1=s1.concat("abc");
+//        System.out.println(s1);
+
+       // System.out.println(s1==s3);
+
+        String s="Hello";
+        //System.out.println(s.charAt(2));
+//        System.out.println(s.concat("Hello"));
+
+//        System.out.println(s.contains("He"));
+//        System.out.println(s.indexOf('e'));
+//        System.out.println((s.lastIndexOf('1')));
+//
+//        System.out.println(s.length());
+//        System.out.println(s.replace('H','A'));
+//        System.out.println(s.toUpperCase());
+//        System.out.println(s.toLowerCase());
+
+
+        // char[] arr=s.toCharArray();
+        // System.out.println(arr);
+        // System.out.println("----");
+        // System.out.println(arr+" hj");
+        // System.out.println(s);
+        // System.out.println(s.trim());
+
+
+    }
+}
